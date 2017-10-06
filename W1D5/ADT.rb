686f880f3c7c8684d3bcdd0def6eab5730002b9a
Jack Wu <jackwu350@yahoo.com>
2017-10-05 22:59:28 -0700

    Implemented Stack, Queue, Map

--- W1D5/ADT.rb
+++ W1D5/ADT.rb
@@ -20,3 +20,58 @@ class Stack
         # return a copy of the stack
       end
     end
+
+class Queue
+  def initialize
+    @queue = Array.new
+  end
+
+  def enqueue(el)
+    @queue.unshift
+
+  end
+
+  def dequeue
+    @queue.shift
+
+  end
+
+end
+
+class Map
+  def initialize
+    @map = Array.new
+  end
+
+  def assign(key,value)
+    @map.each do |pairs|
+      return false if pairs[0] == key
+    end
+    @map << [key,value]
+  end
+
+
+  def lookup(key)
+    @map.each do |pairs|
+      return pairs[1] if pairs[0] == key
+    end
+  end
+
+  def remove(key)
+    @map.each do |pairs|
+      @map.delete(pairs) if pairs[0] == key
+    end
+  end
+
+  def show
+    p @map
+  end
+
+end
+
+
+
+
+
+
+end
