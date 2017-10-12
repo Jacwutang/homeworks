d1b68eea636dea5cf72472e5757dd6e085c4aae3
Jack Wu <jackwu350@yahoo.com>
2017-10-11 21:32:58 -0700

    Finished hw

--- W2D4/octopus.rb
+++ W2D4/octopus.rb
@@ -1,3 +1,45 @@
-def
+#O(N)
+def find_longest_fish
+  fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
 
-end  
+  fish_arr.sort_by {|fish| fish.length}.last
+
+
+
+end
+
+#O(NLogN)
+def find_longest_fish
+  fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+  #run quicksort on this, choose last element
+
+
+
+end
+
+#O(N)
+def find_longest_fish
+  fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+  max = 0
+  fish_arr.each do |fish|
+    if fish.length > max
+      max = fish.length
+    end
+
+  end
+  max
+
+end
+
+
+def slow_dance(direction, tile_arr)
+  #go through the array one el at a time
+
+end
+
+def constant_dance(direction,tile_arr)
+  #use a hash map here
+  
+end
