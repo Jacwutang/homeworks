4c48a42474dd3b2a30f1d4dd239e4e9909635250
Jack Wu <jackwu350@yahoo.com>
2017-11-08 08:37:56 -0800

    finished

---
+++ W6D3/js/index.js
@@ -0,0 +1,11 @@
+console.log("Hello from the JavaScript console!");
+$ajax(
+method: post;
+success: callback();
+url:
+
+
+);
+// Your AJAX request here
+
+// Add another console log here, outside your AJAX request
