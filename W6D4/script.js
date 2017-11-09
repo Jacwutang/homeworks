d4aba1b8d36ff8e5a519c15229c83bfb2195f783
Jack Wu <jackwu350@yahoo.com>
2017-11-08 20:44:29 -0800

    hw

--- W6D4/script.js
+++ W6D4/script.js
@@ -18,11 +18,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
+  const favPlace = document.querySelector(".favorite-input");
+  const favorite = favPlace.value;
+  favPlace.value = "";
+
+  const newListLi = document.createElement("li");
+  newListLi.textContent = favorite;
+
+  const favoritesList = document.createElement("ul")
+  favoritesList.appendChild(newListLi);
+
   // --- your code here!
 
 
 
   // adding new photos
+  const showPhotoForm = (e) => {
+  const photoFormDiv = document.querySelector(".photo-form-container");
+  if (photoFormDiv.className === "photo-form-container") {
+    photoFormDiv.className = "photo-form-container hidden";
+  } else {
+    photoFormDiv.className = "photo-form-container";
+  }
+};
 
   // --- your code here!
 
