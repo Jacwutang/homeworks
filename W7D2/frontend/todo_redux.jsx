33393618c6e2a42332a959eab937097748d0d3ac
Jack Wu <jackwu350@yahoo.com>
2017-11-14 08:24:53 -0800

    hw2

--- W7D2/frontend/todo_redux.jsx
+++ W7D2/frontend/todo_redux.jsx
@@ -4,6 +4,17 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+const addLoggingToDispatch = (store) => {
+  const dispatchVar = store.dispatch;
+
+  return (action) => {
+
+    
+  }
+
+
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
